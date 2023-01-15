21972f4736d8af2ce349e447516afa75dacbb1ce
develrocket <victory20201224@gmail.com>
2023-01-15 13:25:16 +0100

    init

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import PresentationalComponent from './PresentationalComponent'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+    state = {
+        myState: "This is my state"
+    }
+
+    updateState = () => {
+        if (this.state.myState === "This is my state") {
+            this.setState({ myState: 'The state is updated' })
+        } else {
+            this.setState({ myState: 'This is my state' })
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <PresentationalComponent myState = {this.state.myState} updateState = {this.updateState}/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
